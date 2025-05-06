46e644c4b47f9d6829a898a8dfcefe352f66f9b8
Challa Sri Vidya Aiswarya <129404938+csvidya16@users.noreply.github.com>
2025-05-06 16:22:50 +0530

    Add files via upload

---
+++ practice1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class practice1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum of two numbers "+a+" and "+b+" is "+ (a+b));
+        sc.close();
+    }
+    
+}
